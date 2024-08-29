550bd1565773c0c2d03eb873491fff95c36e2fb0
valdez Tedongmo <114952948+TedongmoKanouoValdez@users.noreply.github.com>
2024-08-29 14:34:29 +0200

    creation du projet

---
+++ TP_IONIC/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TP_IONIC',
+  webDir: 'dist'
+};
+
+export default config;
